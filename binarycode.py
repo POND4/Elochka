2937da5932a663e3aef2ea15e53dde9ff681141a
lapis-overdose <lapis.zeymey@gmail.com>
2024-12-14 13:40:20 +0500

    raw concept

--- binarycode.py
+++ binarycode.py
@@ -1,1 +1,17 @@
-print("timur gei")
+def game(hiddenNumber, low, high):
+    if low > high:
+        return "Error"
+    mid = (low + high) // 2
+
+    if hiddenNumber > mid:
+        print("Higher")
+        return game(hiddenNumber, mid + 1, high)
+
+    elif hiddenNumber < mid:
+        print("Lower")
+        return game(hiddenNumber, low, mid - 1)
+    else:
+        return "Game Over"
+
+hiddenNumber = int(input("Enter a number between 1 and 100: "))
+print(game(hiddenNumber, 1, 100))
