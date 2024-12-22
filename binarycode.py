095a3905ec75c4b3a6209d6303556785596eb34c
lapis-overdose <lapis.zeymey@gmail.com>
2024-12-23 00:09:08 +0500

    Testing program body

--- binarycode.py
+++ binarycode.py
@@ -1,6 +1,5 @@
-import random
 
-
+'''
 def game(hiddenNumber, low, high):
     if low > high:
         return "Error"
@@ -25,3 +24,44 @@ hiddenNumber = random.randint(1, 100)
 print("Welcome to the Guessing Game!")
 print("guess a number from 1 to 100.")
 print(game(hiddenNumber, 1, 100))
+
+'''
+
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Guessing Game")
+
+#ICON = pygame.image.load("icon.png")
+
+#pygame.display.set_icon(pygame.image.load(ICON)
+
+#BACKGROUND = pygame.image.load("images/background.jpg")
+
+#FONT = pygame.font.SysFont()
+
+CLOCK = pygame.time.Clock()
+
+
+def test():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        # Код отображения
+
+        SCREEN.fill(("white"))
+        square_size = 100
+        square_position = (WIDTH // 2 - square_size // 2, HEIGHT // 2 - square_size // 2)
+        pygame.draw.rect(SCREEN, "blue", (*square_position, square_size, square_size))
+
+        pygame.display.update()
+
+        CLOCK.tick(30)
+
+test()
