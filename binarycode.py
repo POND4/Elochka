fde984895cd1bd175f1cc8cbe61d77a1784195c4
lapis-overdose <lapis.zeymey@gmail.com>
2024-12-22 17:07:19 +0500

    testing 1

--- binarycode.py
+++ binarycode.py
@@ -1,17 +1,27 @@
+import random
+
+
 def game(hiddenNumber, low, high):
     if low > high:
         return "Error"
+
     mid = (low + high) // 2
+    print(f"Is the number {mid}?")
 
-    if hiddenNumber > mid:
-        print("Higher")
+    answ = input("Enter 'H' for higher, 'L' for lower, or 'Y' if correct: ").strip().lower()
+
+    if answ == "h":
         return game(hiddenNumber, mid + 1, high)
-
-    elif hiddenNumber < mid:
-        print("Lower")
+    elif answ == "l":
         return game(hiddenNumber, low, mid - 1)
+    elif answ == "y":
+        return f"Game Over: The hidden number was {hiddenNumber}!"
     else:
-        return "Game Over"
+        print("Invalid input. Please type 'H', 'L', or 'Y'.")
+        return game(hiddenNumber, low, high)
 
-hiddenNumber = int(input("Enter a number between 1 and 100: "))
+
+hiddenNumber = random.randint(1, 100)
+print("Welcome to the Guessing Game!")
+print("guess a number from 1 to 100.")
 print(game(hiddenNumber, 1, 100))
